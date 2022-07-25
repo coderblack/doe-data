9794a26a221cf59ebe9e6922c5f51c277d59706c
83544844@qq.com <83544844@qq.com>
2022-07-25 18:20:39 +0800

    学大数据到多易教育

--- data-etl/src/main/java/cn/doitedu/etl/AppTrafficFactTableBuilder.scala
+++ data-etl/src/main/java/cn/doitedu/etl/AppTrafficFactTableBuilder.scala
@@ -1,9 +1,9 @@
 package cn.doitedu.etl
 
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SaveMode, SparkSession}
 
-import scala.collection.mutable
+import scala.collection.{immutable, mutable}
 import scala.collection.mutable.ListBuffer
 
 // |guid|session_id|page_id|referPage   |event_time   |session_endtime|
@@ -16,7 +16,7 @@ case class PageBean(
                      session_endtime: Long
                    )
 
-case class ResultBean(guid:Long, session_id:String, page_id:String, pv_cnt:Long, page_staytime:Long, pv_contribute_d:Int, pv_contribute_w:Int)
+case class ResultBean(dt:String ,guid: Long, session_id: String, page_id: String, pv_cnt: Int, page_staytime: Long, pv_contribute_d: Int, pv_contribute_w: Int)
 
 object AppTrafficFactTableBuilder {
 
@@ -26,6 +26,8 @@ object AppTrafficFactTableBuilder {
       .master("local")
       .appName("商城app流量分析-星型模型-事实表计算")
       .enableHiveSupport()
+      .config("spark.sql.shuffle.partitions","2")
+      .config("hive.exec.dynamic.partition.mode","nonstrict")
       .getOrCreate()
 
 
@@ -77,7 +79,7 @@ object AppTrafficFactTableBuilder {
      * PageBean(1846,ZYBFEFJFQW,page043,index,1658043581146,1658043592901)
      * PageBean(1846,ZYBFEFJFQW,page001,index,1658043582115,1658043592901)
      * PageBean(1846,ZYBFEFJFQW,page023,index,1658043583601,1658043592901)
-     * PageBean(1846,ZYBFEFJFQW,page001,page023,1658043583601,1658043592901)
+
      * PageBean(1846,ZYBFEFJFQW,page045,page023,1658043589124,1658043592901)
      */
 
@@ -117,7 +119,7 @@ object AppTrafficFactTableBuilder {
         }
 
         // 每算完一个页面的停留时长，就放入结果hashmap中存储(如果之前已经存在这个页面，则把时间累加
-        pageStaytimeMap += ((pageloadEventList(i).page_id, pageStaytimeMap.getOrElse(pageloadEventList(i).page_id, 0) + staytime))
+        pageStaytimeMap += ((pageloadEventList(i).page_id, pageStaytimeMap.getOrElse(pageloadEventList(i).page_id, 0L) + staytime))
 
       }
 
@@ -130,7 +132,7 @@ object AppTrafficFactTableBuilder {
         if (node == null) {
           node = TreeNode(pageBean.page_id, ListBuffer.empty)
         } else {
-          PageContributeUtil.findAndAppend(node,pageBean.page_id,if(pageBean.referPage == null) "直接访问" else pageBean.referPage)
+          PageContributeUtil.findAndAppend(node, pageBean.page_id, if (pageBean.referPage == null) "直接访问" else pageBean.referPage)
         }
       }
 
@@ -146,17 +148,17 @@ object AppTrafficFactTableBuilder {
 
        */
 
-      val lst = ListBuffer.empty[(String,Int)]
-      PageContributeUtil.calcWholeContributePv(node,lst) // List[(a,6),(b,2),(c,2),(a,1),(x,0)]
+      val lst = ListBuffer.empty[(String, Int)]
+      PageContributeUtil.calcWholeContributePv(node, lst) // List[(a,6),(b,2),(c,2),(a,1),(x,0)]
       val wholeContributeMap: Map[String, Int] =
-        lst.groupBy(tp => tp._1)  // (a,List((a,6),(a,1)))
-        .map(tp => (tp._1, tp._2.map(tp => tp._2).sum))
+        lst.groupBy(tp => tp._1) // (a,List((a,6),(a,1)))
+          .map(tp => (tp._1, tp._2.map(tp => tp._2).sum))
 
 
-      val lst2 = ListBuffer.empty[(String,Int)]
-      PageContributeUtil.calcDirectContributePv(node,lst2) // List[(a,2),(b,2),(c,2),(a,1),(x,0)]
+      val lst2 = ListBuffer.empty[(String, Int)]
+      PageContributeUtil.calcDirectContributePv(node, lst2) // List[(a,2),(b,2),(c,2),(a,1),(x,0)]
       val directContributeMap: Map[String, Int] =
-        lst2.groupBy(tp => tp._1)  // (a,List((a,2),(a,1)))
+        lst2.groupBy(tp => tp._1) // (a,List((a,2),(a,1)))
           .map(tp => (tp._1, tp._2.map(tp => tp._2).sum))
 
 
@@ -166,15 +168,24 @@ object AppTrafficFactTableBuilder {
        * directContributeMap,
        * wholeContributeMap,
        * 这4部分结果，按照相同页面整合拼接 得到最终要的结果：
-       *  guid,session_id,   page_id ?,  pv ?, pageStaytime ?  , 直接贡献量?   总贡献量 ?
+       * guid,session_id,   page_id ?,  pv ?, pageStaytime ?  , 直接贡献量?   总贡献量 ?
        */
-      val resList:ListBuffer[ResultBean] = ListBuffer.empty
-      // TODO
+
+      for (kv <- pagePvs) yield {
+        ResultBean("2022-07-16",tp._1._1, tp._1._2, kv._1, kv._2, pageStaytimeMap.getOrElse(kv._1, 0L), directContributeMap.getOrElse(kv._1, 0), wholeContributeMap.getOrElse(kv._1, 0))
+      }
 
 
-      resList
     })
 
+
+    // 输出结果到hive的目标表中
+    val resultDF = result.toDF()
+    resultDF.write
+      .mode(SaveMode.Overwrite)
+      .format("hive")
+      .partitionBy("dt")
+      .saveAsTable("dws.doitedu_mall_app_traffic_aggr_usp")
 
     spark.close()
 
