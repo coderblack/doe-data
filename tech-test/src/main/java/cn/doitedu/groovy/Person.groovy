2034f98068996c65c07e49960aea58c103f65d12
83544844@qq.com <83544844@qq.com>
2022-08-09 21:21:31 +0800

    学大数据到多易教育

--- tech-test/src/main/java/cn/doitedu/groovy/Person.groovy
+++ tech-test/src/main/java/cn/doitedu/groovy/Person.groovy
@@ -1,16 +1,20 @@
 package cn.doitedu.groovy
 
+import groovy.util.logging.Slf4j
+
+@Slf4j
 class Person {
-   def String sayName1(String name){
+    def String sayName1(String name) {
+        log.error("hahahaha")
         return "hello " + name
     }
 
-    def String sayName2(String name , String suffix){
+    def String sayName2(String name, String suffix) {
         return name + " " + suffix
     }
 
 
-    def int add(int a, int  b){
-        return a+b
+    def int add(int a, int b) {
+        return a + b
     }
 }
