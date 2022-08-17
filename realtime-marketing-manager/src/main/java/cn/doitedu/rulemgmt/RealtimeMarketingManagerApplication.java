dafd3262ddcd125a47d4b3f049e92a2ce6ff4aeb
83544844@qq.com <83544844@qq.com>
2022-08-17 17:56:16 +0800

    学大数据到多易教育

---
+++ realtime-marketing-manager/src/main/java/cn/doitedu/rulemgmt/RealtimeMarketingManagerApplication.java
@@ -0,0 +1,13 @@
+package cn.doitedu.rulemgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeMarketingManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeMarketingManagerApplication.class, args);
+	}
+
+}
